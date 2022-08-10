a5ba9329d4069f6bebe5707f016689938f77bd7c
Nurgak <Nurgak@users.noreply.github.com>
2022-08-10 16:21:54 +0900

    Added OTA capability.

--- firmware/iot_rgb_led_matrix_firmware/config.h
+++ firmware/iot_rgb_led_matrix_firmware/config.h
@@ -1,22 +1,101 @@
+/**
+ \file config.h
+ \brief Configuration file for the IOT RGB LED Matrix.
+ \see https://github.com/Nurgak/IoT-RGB-LED-Matrix-Socket
+*/
 #pragma once
 
-static const char * AP_PASSWORD = "rgbledmatrix";
-static const IPAddress STATIC_IP(192, 168, 3, 101);
-static const IPAddress STATIC_GATEWAY(192, 168, 3, 1);
-static const IPAddress STATIC_SUBNET(255, 255, 255, 0);
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
 
-static const unsigned int SERVER_PORT = 7777;
-static const unsigned int TIMEOUT_MS = 5;
+//static const bool FORMAT_SPIFFS_IF_FAILED true
+static const bool FORMAT_SPIFFS_IF_FAILED = false;
+static const unsigned int WIFI_CONNECT_TIMEOUT = 120;
+static const unsigned int BUFFER_SIZE = 48 * 32 + 1;
+static const unsigned int PIN_CLK = 14;
+static const unsigned int PIN_OE  = 13;
+static const unsigned int PIN_LAT = 15;
+static const unsigned int PIN_A   = 26;
+static const unsigned int PIN_B   = 4;
+static const unsigned int PIN_C   = 27;
+static const unsigned int PIN_D   = 2;
 
-#define WDT_TIMEOUT 120
-#define BUFFER_SIZE 48 * 32 + 1
+static const unsigned int PIN_UPPER_RED   = 5;
+static const unsigned int PIN_UPPER_GREEN = 17;
+static const unsigned int PIN_UPPER_BLUE  = 18;
+static const unsigned int PIN_LOWER_RED   = 19;
+static const unsigned int PIN_LOWER_GREEN = 16;
+static const unsigned int PIN_LOWER_BLUE  = 25;
 
-#define CLK 14
-#define OE  13
-#define LAT 15
-#define A   26
-#define B   4
-#define C   27
-#define D   2
+// Define the upper 3 and lower 3 RGB pins.
+static uint8_t PIN_RGB[] = {
+  PIN_UPPER_RED,
+  PIN_UPPER_GREEN,
+  PIN_UPPER_BLUE,
+  PIN_LOWER_RED,
+  PIN_LOWER_GREEN,
+  PIN_LOWER_BLUE
+};
 
-static uint8_t RGB[] = {5, 17, 18, 19, 16, 25};
+namespace Config
+{
+  /** \brief Static IP of the LED matrix. */
+  char static_ip[16] = "192.168.1.254";
+  /** \brief Static gateway. */
+  char static_gw[16] = "192.168.1.1";
+  /** \brief Static subnet. */
+  char static_sn[16] = "255.255.255.0";
+  /** \brief Server port of the LED matrix. */
+  char port[8] = "7777";
+  /** \brief Restart timeout when no data has been recieved, in seconds. */
+  char timeout[8] = "120";
+
+  void save()
+  {
+    /** \brief Save configuration values to SPIFFS. */
+    DynamicJsonDocument json(256);
+    json["static_ip"] = WiFi.localIP().toString();
+    json["static_gw"] = WiFi.gatewayIP().toString();
+    json["static_sn"] = WiFi.subnetMask().toString();
+    json["port"] = port;
+    json["timeout"] = timeout;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if(!configFile)
+    {
+      return;
+    }
+
+    serializeJson(json, configFile);
+    configFile.close();
+  }
+
+  void load()
+  {
+    /** \brief Load configuration values from SPIFFS. */
+    if(SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
+    {
+      if(SPIFFS.exists("/config.json"))
+      {
+        File configFile = SPIFFS.open("/config.json", "r");
+        if(configFile)
+        {
+          const size_t size = configFile.size();
+          std::unique_ptr<char[]> buf(new char[size]);
+
+          configFile.readBytes(buf.get(), size);
+          DynamicJsonDocument json(256);
+
+          if(DeserializationError::Ok == deserializeJson(json, buf.get()))
+          {
+            strcpy(static_ip, json["static_ip"]);
+            strcpy(static_gw, json["static_gw"]);
+            strcpy(static_sn, json["static_sn"]);
+            strcpy(port, json["port"]);
+            strcpy(timeout, json["timeout"]);
+          }
+        }
+      }
+    }
+  }
+}
--- firmware/iot_rgb_led_matrix_firmware/iot_rgb_led_matrix_firmware.ino
+++ firmware/iot_rgb_led_matrix_firmware/iot_rgb_led_matrix_firmware.ino
@@ -1,54 +1,199 @@
+/**
+ \file
+ \brief Main program file for the IOT RGB LED Matrix.
+ \see https://github.com/Nurgak/IoT-RGB-LED-Matrix-Socket
+*/
 #include <WiFiManager.h>
+#include <ArduinoOTA.h>
+
 #include <esp_task_wdt.h>
 #include <RGBmatrixPanel.h>
 #include <Fonts/FreeSerif9pt7b.h>
+
 #include "config.h"
 
-RGBmatrixPanel matrix(A, B, C, D, CLK, LAT, OE, false, 32, RGB);
+/** \brief Matrix instance, set pin definitions in config.h. */
+RGBmatrixPanel matrix(PIN_A, PIN_B, PIN_C, PIN_D, PIN_CLK, PIN_LAT, PIN_OE, false, 32, PIN_RGB);
 
-static char AP_NAME[24];
-WiFiServer server(SERVER_PORT);
+/** \brief Flag to indicate if configuration should be saved to SPIFFS. */
+bool should_save_config = false;
+/** \brief Unique device indentifier, partially populated with WiFi MAC address. */
+char identifier[24];
+/** \brief Listen task handle, used for pausing/resuming and watchdog checking. */
 TaskHandle_t listen_task_handle;
+
+/** \brief Server instance, listens to the client sending over the display data. */
+WiFiServer server;
 
 void setup()
 {
+  /** \brief Setup function, called once at the beginning of the program. */
   // Needed for WiFi manager debug.
   Serial.begin(115200);
+
+  // Format FS for testing.
+  //SPIFFS.format();
+
+  byte mac[6];
+  WiFi.macAddress(mac);
+  snprintf(identifier, sizeof(identifier), "ESPLEDMATRIX-%02X%02X%02X", mac[5], mac[4], mac[3]);
 
   matrix.begin();
   matrix.print("WiFi\nsetup");
 
-  WiFiManager wifiManager;
+  Config::load();
+  setupWifi();
+  setupOTA();
 
-  //wifiManager.resetSettings();
-  snprintf(AP_NAME, sizeof(AP_NAME), "ESPLEDMatrix-%04X", ESP.getEfuseMac());
+  // Start the display server with the defined port from the configuration.
+  server.begin(atoi(Config::port));
 
-  wifiManager.setSTAStaticIPConfig(STATIC_IP, STATIC_GATEWAY, STATIC_SUBNET);
-  wifiManager.autoConnect(AP_NAME, AP_PASSWORD);
-
-  // Print the IP on the matrix so the client could connect to it.
+  // Print the IP on the matrix so the client could be configured with it.
   matrix.fillScreen(0);
   matrix.setCursor(0, 0);
-  matrix.printf("%d\n%d\n%d\n%d", STATIC_IP[0], STATIC_IP[1], STATIC_IP[2], STATIC_IP[3]);
+  IPAddress ip_address = WiFi.localIP();
+  matrix.printf("%d\n%d\n%d\n%d", ip_address[0], ip_address[1], ip_address[2], ip_address[3]);
 
-  server.begin();
-
+  // Create the listening task on CPU1 and save its handle.
   xTaskCreatePinnedToCore(&listen, "listen", 2048, NULL, 1, &listen_task_handle, 0);
 
-  // Prevent the LED matrix from freezing: restart if no new data has been recieved for 120 seconds.
-  esp_task_wdt_init(WDT_TIMEOUT, true);
+  // Prevent the LED matrix from freezing: restart if no new data has been recieved for a while.
+  esp_task_wdt_init(atoi(Config::timeout), true);
+  // Subscribe the listen task to the watchdog.
   esp_task_wdt_add(listen_task_handle);
+}
+
+void setupOTA()
+{
+  /** \brief Over-the-air update setup function. */
+  ArduinoOTA.onStart([]()
+  {
+    esp_task_wdt_delete(listen_task_handle);
+    vTaskSuspend(listen_task_handle);
+    Serial.println("Start");
+  });
+
+  ArduinoOTA.onEnd([]()
+  {
+    Serial.println("\nEnd");
+    // The ESP will reboot at this point, no need to resume the listen task.
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+  {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+
+    matrix.fillScreen(0);
+    matrix.setCursor(0, 0);
+    matrix.printf("Prog\n%u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error)
+  {
+    Serial.printf("Error[%u]: ", error);
+
+    matrix.fillScreen(0);
+    matrix.setCursor(0, 0);
+    matrix.printf("Error\n%u", error);
+
+    if(error == OTA_AUTH_ERROR)
+    {
+        Serial.println("Auth Failed");
+    }
+    else if (error == OTA_BEGIN_ERROR)
+    {
+        Serial.println("Begin Failed");
+    }
+    else if (error == OTA_CONNECT_ERROR)
+    {
+        Serial.println("Connect Failed");
+    }
+    else if (error == OTA_RECEIVE_ERROR)
+    {
+        Serial.println("Receive Failed");
+    }
+    else if (error == OTA_END_ERROR)
+    {
+        Serial.println("End Failed");
+    }
+
+    // Resume normal operation when an error has occurred.
+    vTaskResume(listen_task_handle);
+    esp_task_wdt_add(listen_task_handle);
+  });
+
+  ArduinoOTA.setHostname(identifier);
+  ArduinoOTA.setPassword(identifier);
+  ArduinoOTA.begin();
+}
+
+void saveConfigCallback()
+{
+  /** \brief Callback function triggered from the WiFi manager front end. */
+  should_save_config = true;
+
+  // Restart the server just in case the user changed the port.
+  server.begin(atoi(Config::port));
+  // Reconfigure the watchdog timeout value in case it was changed.
+  esp_task_wdt_init(atoi(Config::timeout), true);
+}
+
+void setupWifi()
+{
+  /** \brief WiFi setup function. */
+  WiFiManager wifiManager;
+
+  // Reset WiFI settings for testing.
+  //wifiManager.resetSettings();
+
+  WiFiManagerParameter rgbletmatrix_port("port", "Matrix server port", Config::port, sizeof(Config::port));
+  WiFiManagerParameter rgbletmatrix_timeout("timeout", "Listen timeout in seconds", Config::timeout, sizeof(Config::timeout));
+
+  wifiManager.setDebugOutput(false);
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  wifiManager.addParameter(&rgbletmatrix_port);
+  wifiManager.addParameter(&rgbletmatrix_timeout);
+  // Set connection timeout, different from display timeout.
+  wifiManager.setTimeout(WIFI_CONNECT_TIMEOUT);
+
+  IPAddress static_ip, static_gw, static_sn;
+  static_ip.fromString(Config::static_ip);
+  static_gw.fromString(Config::static_gw);
+  static_sn.fromString(Config::static_sn);
+  wifiManager.setSTAStaticIPConfig(static_ip, static_gw, static_sn);
+
+  WiFi.hostname(identifier);
+
+  // Block here while attempting to connect.
+  if(!wifiManager.autoConnect(identifier))
+  {
+    // Restart when WiFi connection unsuccessful after timeout.
+    ESP.restart();
+  }
+
+  strcpy(Config::port, rgbletmatrix_port.getValue());
+  strcpy(Config::timeout, rgbletmatrix_timeout.getValue());
+
+  if(should_save_config)
+  {
+    Config::save();
+  }
+  else
+  {
+    Config::load();
+  }
 }
 
 void listen(void *pvParameter)
 {
-  unsigned long time_start_ms = 0;
+  /** \brief Listen function, updating the matrix display with data recieved from the client. */
   uint8_t * buffer_ptr = matrix.backBuffer();
   size_t bytes_read;
+  WiFiClient client;
 
   while(true)
   {
-    WiFiClient client = server.available();
+    client = server.available();
     if(client)
     {
       while(client.connected())
@@ -70,4 +215,6 @@ void listen(void *pvParameter)
 
 void loop()
 {
+  /** \brief Infinite loop, used for OTA handle call while idle. */
+  ArduinoOTA.handle();
 }
